1afcbba20a3192afd04fdb79d7e32068812435eb
Luis <luis.mangas15@gmail.com>
2021-05-10 01:16:43 +0200

    Primera implementación carro. No se muestra nada en carro.html Se intenta añadir sin errores.

--- src/main/java/curso/java/tienda/controller/ProductoController.java
+++ src/main/java/curso/java/tienda/controller/ProductoController.java
@@ -1,13 +1,18 @@
 package curso.java.tienda.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import curso.java.tienda.model.Producto;
+import curso.java.tienda.model.Usuario;
 import curso.java.tienda.repository.ProductoRepository;
 import curso.java.tienda.service.CategoriaService;
 import curso.java.tienda.service.ProductoService;
@@ -25,7 +30,8 @@ public class ProductoController {
 	public String inicio(Model model,HttpSession session) {
 		
 		model.addAttribute("listaProductos", ps.listadoProductos());
-		session.setAttribute("listaCategorias", cs.listadoCategorias());
+		session.setAttribute("listaCategorias", cs.listadoCategorias());//creo la lista de categorias en el inicio
+		session.setAttribute("carro", new ArrayList<Producto>());//creo el carro en el inicio
 		
 		return "producto/lista";
 	}
@@ -34,8 +40,24 @@ public class ProductoController {
 	public String detalle(Model model,HttpSession session) {
 		
 		model.addAttribute("listaProductos", ps.listadoProductos());
-		session.setAttribute("listaCategorias", cs.listadoCategorias());
 		
 		return "producto/detalle";
+	}
+	
+	@GetMapping("/carro")
+	public String carro(Model model,HttpSession session) {
+		
+		model.addAttribute("lista", session.getAttribute("carro"));
+		
+		return "producto/carro";
+	}
+	
+	
+	@GetMapping("insertar_carro")
+	public void insertarCarro(Model model,HttpSession session, @ModelAttribute Producto producto) {
+//		((ArrayList<Producto>) session.getAttribute("carro")).add(producto);
+		ArrayList<Producto> carro = (ArrayList<Producto>) session.getAttribute("carro");
+ 		carro.add(producto);
+		session.setAttribute("carro", carro);
 	}
 }
